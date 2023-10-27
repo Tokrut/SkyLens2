dcf6a4bb05ed33373c6c3b001d115d5188a0285f
Tokrut <149145356+Tokrut@users.noreply.github.com>
2023-10-27 15:20:46 +0300

    Update input_s.py

--- programmer/input_s.py
+++ programmer/input_s.py
@@ -1,21 +1,28 @@
+from math import *
+from scipy import *
+from sqlite3 import *
+
+connection = connect('testDB2.db')
+curs = connection.cursor()
 def input_d(tec_id):
-    global L1, L2, B1, B2, exentz, malpolz, bolpolz, roz1, x1, y1, z1, roz2, x2, y2, z2
+    global L, B, H, exentz, malpolz, bolpolz, roz1, x, y, z, A, fi, chast
     curs.execute('SELECT B1 FROM Orders WHERE id = '+str(tec_id))
     B1 = int(curs.fetchall()[0][0])
-    curs.execute('SELECT B2 FROM Orders WHERE id = '+str(tec_id))
-    B2 = int(curs.fetchall()[0][0])
     curs.execute('SELECT L1 FROM Orders WHERE id = '+str(tec_id))
     L1 = int(curs.fetchall()[0][0])
-    curs.execute('SELECT L2 FROM Orders WHERE id = '+str(tec_id))
-    L2 = int(curs.fetchall()[0][0])
-    malpolz = 6356.777
-    bolpolz = 6378.160
+    curs.execute('SELECT L1 FROM Orders WHERE id = ' + str(tec_id))
+    A = int(curs.fetchall()[0][0])
+    curs.execute('SELECT L1 FROM Orders WHERE id = ' + str(tec_id))
+    fi = int(curs.fetchall()[0][0])
+    curs.execute('SELECT L1 FROM Orders WHERE id = ' + str(tec_id))
+    ugspid = int(curs.fetchall()[0][0])
+    malpolz = 6356.777+H
+    bolpolz = 6378.160+H
     exentz = 0.0167
     roz1 = malpolz/(1-exentz**2*sin(B1)**2)
-    x1 = roz1*cos(B1)*cos(L1)
-    y1 = roz1*cos(B1)*sin(L1)
-    z1 = (bolpolz**2/malpolz**2)*roz1*sin(B1)
-    roz2 = malpolz/(1-exentz**2*sin(B2)**2)
-    x2 = roz2*cos(B2)*cos(L2)
-    y2 = roz2*cos(B2)*sin(L2)
-    z2 = (bolpolz**2/malpolz**2)*roz2*sin(B2)
+    x = roz1*cos(B1)*cos(L1)
+    y = roz1*cos(B1)*sin(L1)
+    z = (bolpolz**2/malpolz**2)*roz1*sin(B1)
+    chast = ugspid/(2*pi)
+    period = 1 / chast
+    A*sin(chast*t+fi)
