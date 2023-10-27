72a4d6c25a6c6dd07b7b486309edec3f320d99bc
Tokrut <149145356+Tokrut@users.noreply.github.com>
2023-10-27 17:47:41 +0300

    Update input_s.py

--- programmer/input_s.py
+++ programmer/input_s.py
@@ -4,7 +4,7 @@ from sqlite3 import *
 #connection = connect('testDB2.db')
 #curs = connection.cursor()
 def input_d(tec_id):
-    global L, B, H, exentz, malpolz, bolpolz, roz1, x, y, z, Amplitude, nachfiz, chast, period, t0, ugolcam
+    global L, B, H, exentz, malpolz, bolpolz, roz1, x, y, z, Amplitude, nachfiz, chast, period, t0, ugolcam, obl
 #    curs.execute('SELECT B1 FROM Orders WHERE id = '+str(tec_id))
     B = float(input())#'curs.fetchall()[0][0]'
  #   curs.execute('SELECT L1 FROM Orders WHERE id = '+str(tec_id))
@@ -17,25 +17,28 @@ def input_d(tec_id):
     t0 = float(input())  # curs.fetchall()[0][0]
     #  curs.execute('SELECT L1 FROM Orders WHERE id = ' + str(tec_id))
     ugolcam = float(input())  # curs.fetchall()[0][0]
-    bolpolz = 6378.160 + H
     ugspid = float(input())/(bolpolz)#curs.fetchall()[0][0]
-    malpolz = 6356.777 + H
-    exentz = 0.0167
-    Amplitude = 10001.965
     roz1 = malpolz / ( 1 - exentz ** 2 * sin(B) ** 2 )
     x = roz1*cos(B)*cos(L)
     y = roz1*cos(B)*sin(L)
     z = (bolpolz**2/malpolz**2)*roz1*sin(B)
     chast = ugspid/(2*pi)
-    period = 1 / chast
-    t0 %= 40075.017
-    y_s = Amplitude*sin(chast*t0+nachfiz)
-    obl = tan(ugolcam) * H * 2
-    if t0<20037.5085:
-        B=(t0-20037.5085)/111.31949166666666
-    else:
-        t0-=20037.5085
-        B = t0 / 111.31949166666666
-    L = y_s / 111.13294444444445
-    
+    obl = tan(ugolcam) * H
+def progon():
+    for t in range(0,86400,60):
+        t0 = t%40075.017
+        y_s = Amplitude * sin(chast * t0 + nachfiz)
+        if t0 < 20037.5085:
+            B = (t0 - 20037.5085) / 111.31949166666666
+        else:
+            t0 -= 20037.5085
+            B = t0 / 111.31949166666666
+        L = y_s / 111.13294444444445
+        if (B+obl >= B2 and L+obl >= L2) and (B-obl <= B1 and L-obl <= L1):
+            return t
+            break
+malpolz = 6356.777 + H
+exentz = 0.0167
+Amplitude = 10001.965
+bolpolz = 6378.160 + H
 input_d(1)
