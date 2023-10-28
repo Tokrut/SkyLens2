191fae56b6df206fb8d2ca1026d87018404928c7
Zimplay <106824103+Zimplay@users.noreply.github.com>
2023-10-28 10:55:25 +0300

    Update Database.py

--- programmer/Database.py
+++ programmer/Database.py
@@ -9,36 +9,48 @@ try:
         database = 'testmydb',
     ) as connection:
         with connection.cursor() as curs:
+#            curs.execute('''
+#            CREATE TABLE Sput (
+#                id INT PRIMARY KEY,
+#                B TEXT,
+#                L TEXT,
+#                H TEXT,
+#                fi0 TEXT,
+#                t0 TEXT,
+#                Omega TEXT,
+#                alf_cam TEXT
+#            )
+#            ''')
+#            curs.execute('''
+#            CREATE TABLE Users (
+#                id INT AUTO_INCREMENT PRIMARY KEY,
+#                login VARCHAR(75),
+#                psw VARCHAR(255),
+#                email VARCHAR(100),
+#                date_podp DATE
+#            )
+#            ''')
+#            curs.execute('''
+#            CREATE TABLE Orders (
+#                id INT AUTO_INCREMENT PRIMARY KEY,
+#                id_user INT,
+#                B1 TEXT,
+#                L1 TEXT,
+#                B2 TEXT,
+#                L2 TEXT,
+#                FOREIGN KEY (id_user) REFERENCES Users(id)
+#            )
+#            ''')
             curs.execute('''
-            CREATE TABLE Sput (
-                id INT PRIMARY KEY,
-                B TEXT,
-                L TEXT,
-                H TEXT,
-                fi0 TEXT,
-                t0 TEXT,
-                Omega TEXT,
-                alf_cam TEXT
-            )
-            ''')
-            curs.execute('''
-            CREATE TABLE Users (
+            CREATE TABLE Compl_Orders (
                 id INT AUTO_INCREMENT PRIMARY KEY,
-                login VARCHAR(75),
-                psw VARCHAR(255),
-                email VARCHAR(100),
-                date_podp DATE
-            )
-            ''')
-            curs.execute('''
-            CREATE TABLE Orders (
-                id INT AUTO_INCREMENT PRIMARY KEY,
-                id_user INT,
+                id_order INT,
                 B1 TEXT,
                 L1 TEXT,
                 B2 TEXT,
                 L2 TEXT,
-                FOREIGN KEY (id_user) REFERENCES Users(id)
+                Photo BLOB,
+                FOREIGN KEY (id_order) REFERENCES Orders(id)
             )
             ''')
             connection.commit()
